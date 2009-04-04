e036dedb4bf4fa525aff6bdfa8e84a75877151bf
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2009-04-04 20:48:12 +0300

    Fixed RSS parsing. Added correct character set conversion tables for ISO-8859-2, ISO-8859-15 and CP1250.

--- parser.c
+++ parser.c
@@ -87,17 +87,96 @@ int depth = 0;
 char data_string[LONG_TEXT_LEN];
 std::stack<struct XmlNode> nodestack;
 
-static int XMLCALL UnknownEncodingHandler(void *data,const XML_Char *encoding, XML_Encoding *info)
+// Character set mappings can be found at:
+// ftp://ftp.unicode.org/Public/MAPPINGS/
+
+// ISO-8859-15
+static int MapLatin15[0x80] = {
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+  0x00a0, 0x00a1, 0x00a2, 0x00a3, 0x20ac, 0x00a5, 0x0160, 0x00a7,
+  0x0161, 0x00a9, 0x00aa, 0x00ab, 0x00ac, 0x00ad, 0x00ae, 0x00af,
+  0x00b0, 0x00b1, 0x00b2, 0x00b3, 0x017d, 0x00b5, 0x00b6, 0x00b7,
+  0x017e, 0x00b9, 0x00ba, 0x00bb, 0x0152, 0x0153, 0x0178, 0x00bf,
+  0x00c0, 0x00c1, 0x00c2, 0x00c3, 0x00c4, 0x00c5, 0x00c6, 0x00c7,
+  0x00c8, 0x00c9, 0x00ca, 0x00cb, 0x00cc, 0x00cd, 0x00ce, 0x00cf,
+  0x00d0, 0x00d1, 0x00d2, 0x00d3, 0x00d4, 0x00d5, 0x00d6, 0x00d7,
+  0x00d8, 0x00d9, 0x00da, 0x00db, 0x00dc, 0x00dd, 0x00de, 0x00df,
+  0x00e0, 0x00e1, 0x00e2, 0x00e3, 0x00e4, 0x00e5, 0x00e6, 0x00e7,
+  0x00e8, 0x00e9, 0x00ea, 0x00eb, 0x00ec, 0x00ed, 0x00ee, 0x00ef,
+  0x00f0, 0x00f1, 0x00f2, 0x00f3, 0x00f4, 0x00f5, 0x00f6, 0x00f7,
+  0x00f8, 0x00f9, 0x00fa, 0x00fb, 0x00fc, 0x00fd, 0x00fe, 0x00ff
+};
+
+// ISO-8859-2
+static int MapLatin2[0x80] = {
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+      -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,
+  0x00a0, 0x0104, 0x02d8, 0x0141, 0x00a4, 0x013d, 0x015a, 0x00a7,
+  0x00a8, 0x0160, 0x015e, 0x0164, 0x0179, 0x00ad, 0x017d, 0x017b,
+  0x00b0, 0x0105, 0x02db, 0x0142, 0x00b4, 0x013e, 0x015b, 0x02c7,
+  0x00b8, 0x0161, 0x015f, 0x0165, 0x017a, 0x02dd, 0x017e, 0x017c,
+  0x0154, 0x00c1, 0x00c2, 0x0102, 0x00c4, 0x0139, 0x0106, 0x00c7,
+  0x010c, 0x00c9, 0x0118, 0x00cb, 0x011a, 0x00cd, 0x00ce, 0x010e,
+  0x0110, 0x0143, 0x0147, 0x00d3, 0x00d4, 0x0150, 0x00d6, 0x00d7,
+  0x0158, 0x016e, 0x00da, 0x0170, 0x00dc, 0x00dd, 0x0162, 0x00df,
+  0x0155, 0x00e1, 0x00e2, 0x0103, 0x00e4, 0x013a, 0x0107, 0x00e7,
+  0x010d, 0x00e9, 0x0119, 0x00eb, 0x011b, 0x00ed, 0x00ee, 0x010f,
+  0x0111, 0x0144, 0x0148, 0x00f3, 0x00f4, 0x0151, 0x00f6, 0x00f7,
+  0x0159, 0x016f, 0x00fa, 0x0171, 0x00fc, 0x00fd, 0x0163, 0x02d9
+};
+
+// Windows CP1250
+static int MapCp1250[0x80] = {
+  0x20ac,     -1, 0x201a,     -1, 0x201e, 0x2026, 0x2020, 0x2021,
+      -1, 0x2030, 0x0160, 0x2039, 0x015a, 0x0164, 0x017d, 0x0179,
+      -1, 0x2018, 0x2019, 0x201c, 0x201d, 0x2022, 0x2013, 0x2014,
+      -1, 0x2122, 0x0161, 0x203a, 0x015b, 0x0165, 0x017e, 0x017a,
+  0x00a0, 0x02c7, 0x02d8, 0x0141, 0x00a4, 0x0104, 0x00a6, 0x00a7,
+  0x00a8, 0x00a9, 0x015e, 0x00ab,     -1, 0x00ad, 0x00ae, 0x017b,
+  0x00b0, 0x00b1, 0x02db, 0x0142, 0x00b4, 0x00b5, 0x00b6, 0x00b7,
+  0x00b8, 0x0105, 0x015f, 0x00bb, 0x013d, 0x02dd, 0x013e, 0x017c,
+  0x0154, 0x00c1, 0x00c2, 0x0102, 0x00c4, 0x0139, 0x0106, 0x00c7,
+  0x010c, 0x00c9, 0x0118, 0x00cb, 0x011a, 0x00cd, 0x00ce, 0x010e,
+  0x0110, 0x0143, 0x0147, 0x00d3, 0x00d4, 0x0150, 0x00d6, 0x00d7,
+  0x0158, 0x016e, 0x00da, 0x0170, 0x00dc, 0x00dd, 0x0162, 0x00df,
+  0x0155, 0x00e1, 0x00e2, 0x0103, 0x00e4, 0x013a, 0x0107, 0x00e7,
+  0x010d, 0x00e9, 0x0119, 0x00eb, 0x011b, 0x00ed, 0x00ee, 0x010f,
+  0x0111, 0x0144, 0x0148, 0x00f3, 0x00f4, 0x0151, 0x00f6, 0x00f7,
+  0x0159, 0x016f, 0x00fa, 0x0171, 0x00fc, 0x00fd, 0x0163, 0x02d9
+};
+
+static int XMLCALL UnknownEncodingHandler(void *data, const XML_Char *encoding, XML_Encoding *info)
 {
+  int *map = NULL;
+
   if (strcmp(encoding, "iso-8859-15") == 0) {
+     map = MapLatin15;
+     }
+  if (strcmp(encoding, "iso-8859-2") == 0) {
+     map = MapLatin2;
+     }
+  else if (strcmp(encoding, "windows-1250") == 0) {
+     map = MapCp1250;
+     }
+
+  if (map) {
      int i;
-     for (i = 0; i < 256; ++i)
-        info->map[i] = i;
-     info->data = NULL;
-     info->convert = NULL;
-     info->release = NULL;
+     for (i = 0; i < 0x80; ++i) {
+         info->map[i] = i;
+         info->map[i + 0x80] = map[i];
+         }
+     info->map[0x7f] = -1;
+     info->data      = NULL;
+     info->convert   = NULL;
+     info->release   = NULL;
      return XML_STATUS_OK;
      }
+
   return XML_STATUS_ERROR;
 }
 
@@ -109,7 +188,7 @@ static void XMLCALL StartHandler(void *data, const char *el, const char **attr)
   node.depth = depth;
   nodestack.push(node);
 
-  if (!strncmp(el, "item", 4)) {
+  if (!strncmp(el, "item", 4) || !strncmp(el, "entry", 5)) {
      cItem *tmpitem = new cItem;
      item = tmpitem;
      item->Clear();
@@ -132,49 +211,29 @@ static void XMLCALL EndHandler(void *data, const char *el)
   // No need to free the node
   
   depth--;
-  if (!strncmp(el, "item", 4)) {
+  if (!strncmp(el, "item", 4) || !strncmp(el, "entry", 5)) {
      // End of the current item
      if (*item->GetTitle())
         Parser.Items.Add(item);
      }
   else if (!strncmp(el, "title", 5)) {
-     if (!strncmp(parent, "item", 4)) {
+     if (!strncmp(parent, "item", 4) || !strncmp(parent, "entry", 5)) {
         item->SetTitle(data_string);
-        }
-     else if (!strncmp(parent, "channel", 7)) {
-        debug("cParser::EndHandler(): RSS title '%s'", data_string);
         }
      }
   else if (!strncmp(el, "link", 4)) {
-     if (!strncmp(parent, "item", 4)) {
+     if (!strncmp(parent, "item", 4) || !strncmp(parent, "entry", 5)) {
         item->SetLink(data_string);
         }
-     else if (!strncmp(parent, "channel", 7)) {
-        debug("cParser::EndHandler(): RSS link '%s'", data_string);
-        }
      }
-  else if (!strncmp(el, "pubDate", 7)) {
-     if (!strncmp(parent, "item", 4)) {
+  else if (!strncmp(el, "pub", 3)) {
+     if (!strncmp(parent, "item", 4) || !strncmp(parent, "entry", 5)) {
         item->SetDate(data_string);
         }
-     else if (!strncmp(parent, "channel", 7)) {
-        debug("cParser::EndHandler(): RSS date '%s'", data_string);
-        }
      }
-  else if (!strncmp(el, "description", 11)) {
-     if (!strncmp(parent, "item", 4)) {
+  else if (!strncmp(el, "description", 11) || !strncmp(el, "content", 7)) {
+     if (!strncmp(parent, "item", 4) || !strncmp(parent, "entry", 5)) {
         item->SetDescription(data_string);
-        }
-     else if (!strncmp(parent, "channel", 7)) {
-        debug("cParser::EndHandler(): RSS description '%s'", data_string);
-        }
-     }
-  else if (!strncmp(el, "content:encoded", 15)) {
-     if (!strncmp(parent, "item", 4)) {
-        item->SetDescription(data_string); // overdrive description with content:encoded !
-        }
-     else if (!strncmp(parent, "channel", 7)) {
-        debug("cParser::EndHandler(): RSS content '%s'", data_string);
         }
      }
   strcpy(data_string, "");
